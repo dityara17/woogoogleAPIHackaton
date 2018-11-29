a98bc213de8f60202fa9a25d59cb7205bb7e65d5
Qampus <hasan.shobri@outlook.com>
2018-11-29 22:34:43 +0700

    Pembuatan home

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@
 //    return view('welcome');
 //});
 
+Route::get('app/admin','WEB\Admin\UserController@index');
+Route::get('app/admin/login','WEB\Admin\AuthController@index');
+Route::post('app/admin/login','WEB\Admin\AuthController@checkLog');
+
 Route::get('/', 'Web\Home\HomeController@index')->name('home');
-Route::get('/course', 'Web\Home\HomeController@course')->name('course');
+Route::get('course', 'Web\Home\HomeController@course')->name('course');
