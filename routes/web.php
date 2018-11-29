5b2606a823d0fc1e47d78e583a8c551fb6e93d63
dityara17 <arizki717@gmail.com>
2018-11-29 22:10:37 +0700

    admin first commit

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 
-Route::get('/', 'Web\Home\HomeController@index')->name('home');
-Route::get('/course', 'Web\Home\HomeController@course')->name('course');
+Route::get('app/admin','WEB\Admin\UserController@index');
+Route::get('app/admin/login','WEB\Admin\AuthController@index');
+Route::post('app/admin/login','WEB\Admin\AuthController@checkLog');
