a26c2852dbbf83a58792dbd3d8c2850b193c85a3
Qampus <hasan.shobri@outlook.com>
2018-11-30 22:26:49 +0700

    Pembuatan home

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,50 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('app/admin/login','WEB\Admin\AuthController@index');
+Route::post('app/admin/login','WEB\Admin\AuthController@checkLog');
+
+Route::group(['middleware' => 'cAuth'], function(){
+
+    //kategori
+    Route::get('app/admin','WEB\Admin\UserController@index');
+    Route::get('app/admin/kategori','WEB\Admin\KategoriController@index');
+    Route::post('app/admin/kategori/add','WEB\Admin\KategoriController@store');
+    Route::get('app/admin/kategori/{kategori_id}/edit','WEB\Admin\KategoriController@edit');
+    Route::get('app/admin/kategori/{kategori_id}/delete','WEB\Admin\KategoriController@destroy');
+    Route::post('app/admin/kategori/{kategori_id}/edit','WEB\Admin\KategoriController@update');
+
+    //kelas
+    Route::get('app/admin/kelas/','WEB\Admin\KelasController@index');
+    Route::get('app/admin/kelas/add','WEB\Admin\KelasController@insert');
+    Route::post('app/admin/kelas/add','WEB\Admin\KelasController@store');
+    Route::get('/app/admin/kelas/{kelas_id}/edit','WEB\Admin\KelasController@edit');
+    Route::post('/app/admin/kelas/{kelas_id}/edit','WEB\Admin\KelasController@update');
+    Route::get('/app/admin/kelas/{kelas_id}/delete','WEB\Admin\KelasController@destroy');
+
+    //kelas konten
+    Route::get('app/admin/kelas/{kelas_id}/materi/','WEB\Admin\KelasKontenController@index');
+    Route::get('app/admin/kelas/{kelas_id}/materi/add','WEB\Admin\KelasKontenController@insert');
+    Route::post('app/admin/kelas/{kelas_id}/materi/add','WEB\Admin\KelasKontenController@store');
+    Route::get('app/admin/kelas/{kelas_id}/materi/{materi_id}/edit','WEB\Admin\KelasKontenController@edit');
+    Route::post('app/admin/kelas/{kelas_id}/materi/{materi_id}/edit','WEB\Admin\KelasKontenController@update');
+    Route::get('app/admin/kelas/{kelas_id}/materi/{materi_id}/delete','WEB\Admin\KelasKontenController@destroy');
+
 });
+
+
+
+Route::get('app/admin', 'WEB\Admin\UserController@index');
+Route::get('app/admin/login', 'WEB\Admin\AuthController@index');
+Route::post('app/admin/login', 'WEB\Admin\AuthController@checkLog');
+
+Route::get('/', 'Web\Home\HomeController@index')->name('home');
+Route::get('class', 'Web\Home\HomeController@course')->name('course');
+Route::get('class/{kategori_id}', 'Web\Home\HomeController@getclass')->name('getclass');
+Route::get('class/detail/{id}','Web\Home\HomeController@detailClass')->name('detailkelas');
+Route::get('class/content/{kelas_id}/{id}','Web\Home\HomeController@kelasKonten')->name('kelaskonten');
